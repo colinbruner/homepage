fb8ec96f85c3821ddd704e1a552e46043bc5e348
Colin Bruner <colin.d.bruner@gmail.com>
2020-07-03 15:53:12 -0400

    add workspaces to circleci

--- build.sh
+++ build.sh
@@ -10,5 +10,11 @@ fi
 
 wget -c https://github.com/getzola/zola/releases/download/${ZOLA}/zola-${ZOLA}-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
 
+echo "Creating workspace dir"
+mkdir -p workspace
+
 echo "Generating Site"
 ./zola -r site build
+
+echo "Moving 'public/' into 'workspace/'"
+mv public workspace/
