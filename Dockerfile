793edac01996d0ed16977a6eb82c35f6d9fcd534
Colin Bruner <colin.d.bruner@gmail.com>
2020-07-25 11:29:35 -0400

    updates to docker ignore and build.sh

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
+#FROM nginx:stable
 FROM arm32v7/nginx:stable
 COPY workspace/public /srv/public
 COPY nginx/config/prod.conf /etc/nginx/conf.d/default.conf
+#COPY nginx/config/dev.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80 443
--- build.sh
+++ build.sh
@@ -2,19 +2,25 @@
 
 # Intended to be run on a Debian variant
 ZOLA="v0.11.0"
+ARCH=$(uname)
 
-if ! $(which wget &>/dev/null); then
-    echo "Installing wget"
-    apt update && apt install -y wget
+echo "Cleaning then && creating workspace dir"
+rm -rf workspace && mkdir -p workspace
+
+if [[ ARCH == 'Linux' ]]; then
+    if ! $(which wget &>/dev/null); then
+        echo "Installing wget"
+        apt update && apt install -y wget
+    fi
+
+    wget -c https://github.com/getzola/zola/releases/download/${ZOLA}/zola-${ZOLA}-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
+    echo "Generating Site"
+    ./zola -r site build
+else
+    # Assume zola is already installed in PATH on Darwin...
+    echo "Generating Site"
+    zola -r site build
 fi
-
-wget -c https://github.com/getzola/zola/releases/download/${ZOLA}/zola-${ZOLA}-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
-
-echo "Creating workspace dir"
-mkdir -p workspace
-
-echo "Generating Site"
-./zola -r site build
 
 echo "Moving 'public/' into 'workspace/'"
 mv public workspace/
