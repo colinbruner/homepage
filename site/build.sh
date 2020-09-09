06b30606cbb01d3d58ed4a596d68661711d6c4cb
Colin Bruner <colin.d.bruner@gmail.com>
2020-09-09 15:46:58 -0400

    update build.sh

--- site/build.sh
+++ site/build.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 # Intended to be run on a Debian variant
-ZOLA="v0.11.0"
+ZOLA="v0.12.0"
 ARCH=$(uname)
 
 echo "Cleaning then && creating workspace dir"
 rm -rf workspace && mkdir -p workspace
 
+# Install Zola on Linux
 if [[ $ARCH == 'Linux' ]]; then
     if ! $(which wget &>/dev/null); then
         echo "Installing wget"
-        apt update && apt install -y wget
+        apt update -y && apt install -y wget
     fi
 
-    echo "Downloading Zola binary"
-    wget -c https://github.com/getzola/zola/releases/download/${ZOLA}/zola-${ZOLA}-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
-    echo "Generating Site"
-    ./zola -r site build
-else
-    # Assume zola is already installed in PATH on Darwin...
-    echo "Generating Site"
-    zola -r site build
+    if ! $(which zola &>/dev/null); then
+        echo "Downloading Zola binary"
+        wget -c https://github.com/getzola/zola/releases/download/${ZOLA}/zola-${ZOLA}-x86_64-unknown-linux-gnu.tar.gz -O - | tar -xz
+        mv zola /usr/bin/zola
+    fi
 fi
+
+# Assume zola is already installed in PATH on Darwin...
+echo "Generating Site"
+zola build
+
+# Remove high def images from travel
+echo "Removing non-processed images"
+find public/travel -name "*.png" -exec rm -f {} \;
 
 echo "Moving 'public/' into 'workspace/'"
 mv public workspace/
-find workspace/public -name "*.png" -exec rm -f {} \;
