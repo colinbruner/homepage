eb7a0362012f823360251d728abf00ba6e3a97b2
Colin Bruner <colin.d.bruner@gmail.com>
2020-09-11 21:44:33 -0400

    fix syntax error w/ pwd

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,7 +11,7 @@ if [[ -f "workspace/public" ]]; then
     cd workspace/public
     aws s3 sync . s3://s3.colinbruner.com/
 else
-    echo "Current path: $(PWD)"
+    echo "Current path: $PWD"
     echo "Unable to find site/workspace/public directory. Were site assets generated?"
 fi
 
