94b4f7cb8e27034f711a10873f9b7fd7cc76f637
Colin Bruner <colin.d.bruner@gmail.com>
2020-09-12 11:54:48 -0400

    sync to specific directory

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,19 +1,19 @@
-#!/bin/bash -e
+#!/bin/bash -eo pipefail
 
-BRANCH=$(git symbolic-ref --short HEAD)
+branch=$(git symbolic-ref --short -q HEAD)
 
 if ! $(which aws &>/dev/null); then
-    # We'll assume the running image has `pip` installed
+    # We'll assume the running image has `pip3` installed
     pip3 install awscli --upgrade --user
 fi
 
-# The generated public directory gets mounted in the root of this directory under
-# a 'workspace/' directory after the generate site stage of circleci
+# The generated public directory gets attached to the root repo directory under
+# 'workspace/' after the generate site stage of circleci has completed.
 if [[ -d "workspace/public/" ]]; then
     cd workspace/public/
-    aws s3 sync . s3://s3.colinbruner.com/
+    # Sync diffs up to the appropriate directory
+    aws s3 sync . s3://s3.colinbruner.com/${branch}
 else
-    echo "Current path: $PWD"
-    echo "Unable to find workspace/public directory. Were site assets generated?"
+    echo "Unable to find workspace/public directory... Were site assets generated?"
 fi
 
