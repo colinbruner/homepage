8e9059f00e83a6cd31cce2c4dfb8e0498328c78b
Colin Bruner <colin.d.bruner@gmail.com>
2020-09-14 21:09:02 -0400

    update sync.sh to use $subdomain.$bucket

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,9 +11,9 @@ fi
 # 'workspace/' after the generate site stage of circleci has completed.
 if [[ -d "workspace/public/" ]]; then
     cd workspace/public/
-    echo "Syncing to s3.colinbruner.com/${branch}"
+    echo "Syncing to s3 bucket ${branch}.colinbruner.com"
     # Sync diffs up to the appropriate directory
-    aws s3 sync . s3://s3.colinbruner.com/${branch}
+    aws s3 sync . s3://${branch}.colinbruner.com
 else
     echo "Unable to find workspace/public directory... Were site assets generated?"
 fi
